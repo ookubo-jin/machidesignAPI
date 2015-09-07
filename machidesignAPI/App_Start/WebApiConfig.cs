5e6c4c9ded037b7668692ddf7348f449e3b58f13
jin <jemini777@gmail.com>
2015-09-08 01:04:27 +0900

    ここらで

--- machidesignAPI/App_Start/WebApiConfig.cs
+++ machidesignAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace machidesignAPI
 {
@@ -10,8 +13,15 @@ namespace machidesignAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API の設定およびサービス
+
+            //http://miso-soup3.hateblo.jp/entry/2013/07/05/065648
             //config.Formatters.XmlFormatter.AddQueryStringMapping("format", "xml", "application/xml");
             //config.Formatters.JsonFormatter.AddQueryStringMapping("format", "json", "application/json");
+            config.Formatters.XmlFormatter.AddUriPathExtensionMapping("xml", "application/xml");
+            config.Formatters.JsonFormatter.AddUriPathExtensionMapping("json", "application/json");
+
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
 
             // Web API ルート
             config.MapHttpAttributeRoutes();
