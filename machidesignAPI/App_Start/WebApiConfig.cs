8e378f7619776da10e9ae0eae6105e952bb25ea5
jin <jemini777@gmail.com>
2015-09-08 01:20:39 +0900

    DB追加

--- machidesignAPI/App_Start/WebApiConfig.cs
+++ machidesignAPI/App_Start/WebApiConfig.cs
@@ -14,21 +14,35 @@ namespace machidesignAPI
         {
             // Web API の設定およびサービス
 
+            //参考サイト
             //http://miso-soup3.hateblo.jp/entry/2013/07/05/065648
-            config.Formatters.XmlFormatter.AddQueryStringMapping("format", "xml", "application/xml");
-            config.Formatters.JsonFormatter.AddQueryStringMapping("format", "json", "application/json");
-            //config.Formatters.XmlFormatter.AddUriPathExtensionMapping("xml", "application/xml");
-            //config.Formatters.JsonFormatter.AddUriPathExtensionMapping("json", "application/json");
 
+            //QueryString で指定
+            //config.Formatters.XmlFormatter.AddQueryStringMapping("format", "xml", "application/xml");
+            //config.Formatters.JsonFormatter.AddQueryStringMapping("format", "json", "application/json");
+
+            //URL の一部で指定
+            config.Formatters.XmlFormatter.AddUriPathExtensionMapping("xml", "application/xml");
+            config.Formatters.JsonFormatter.AddUriPathExtensionMapping("json", "application/json");
+
+            //DBに外部キーがあるとエラーになる対策のはずが解決せず
             //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             //config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
 
             // Web API ルート
             config.MapHttpAttributeRoutes();
 
+            ////QueryString で指定
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            //URL の一部で指定 最後に/を入れないと404になるので注意
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}.{ext}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
